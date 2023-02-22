107b0d17f5f1a351f081e71c67da9198095f558a
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-02-21 22:26:32 -0600

    Navbar image added, React Router setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../images/logo.png'
 
 const Navbar = () => {
     return (
         <nav>
             <ul className='navbar'>
-                <li>
-                    <img src='../images/logo192.png' alt='Logo' />
-                </li>
-                <li><Link>Login</Link></li>
-                <li><Link>Home</Link></li>
-                <li><Link>About</Link></li>
-                <li><Link>Menu</Link></li>
-                <li><Link>Reservations</Link></li>
-                <li><Link>Order Online</Link></li>
+                <li><img src={logo} /></li>
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/menu'>Menu</Link></li>
+                <li><Link to='/reservations'>Reservations</Link></li>
+                <li><Link to='/order-online'>Order Online</Link></li>
             </ul>
         </nav>
     )
