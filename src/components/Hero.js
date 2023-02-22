e2efe33c52df22a595fa77a44d899d3ee3fb3e54
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-02-21 23:29:45 -0600

    Add responsive hamburger dropdown menu to Navbar, and setup layout for Hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,18 @@
+import { Box, Button, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
+import image from '../images/hero.png'
 
 const Hero = () => {
     return (
-        <div>
-            <div>
-                <p>Little Lemon</p>
-                <p>Chicago</p>
-                <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-                <button>Reserce a Table</button>
-            </div>
-            <img src="" alt='Little Lemon dish' />
-        </div>
+        <Box bg='brand.primary1' display='flex' justifyContent='space-evenly' p={5}>
+            <VStack spacing={4} m='auto' w={200}>
+                <Text color='brand.primary2' fontWeight='extrabold' fontSize={64}>Little Lemon</Text>
+                <Text color='brand.highlight1' fontWeight='bold' fontSize={40}>Chicago</Text>
+                <Text color='brand.highlight1'>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</Text>
+                <Button bg='brand.primary2' p={2} borderRadius={16} fontWeight='bold'>Reserve a Table</Button>
+            </VStack>
+            <img src={image} alt='Little Lemon dish' />
+        </Box>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,98 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../images/logo.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+    Box,
+    Button,
+    Flex,
+    HStack,
+    IconButton,
+    Image,
+    Link as ChakraLink,
+    Spacer,
+    useDisclosure,
+} from '@chakra-ui/react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import logo from '../images/logo.png';
 
 const Navbar = () => {
-    return (
-        <nav>
-            <ul className='navbar'>
-                <li><img src={logo} /></li>
-                <li><Link to='/login'>Login</Link></li>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/menu'>Menu</Link></li>
-                <li><Link to='/reservations'>Reservations</Link></li>
-                <li><Link to='/order-online'>Order Online</Link></li>
-            </ul>
-        </nav>
-    )
-}
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-export default Navbar
+    return (
+        <Box bg='gray.100' py={3}>
+            <Flex align='center' justify='space-between' maxW='900px' mx='auto'>
+                <Flex align='center' ml={2}>
+                    <Image src={logo} alt='Logo' maxH='50px' />
+                </Flex>
+                <Flex justify='flex-end' w='50%' display={{ base: 'none', md: 'flex' }}>
+                    <ChakraLink as={Link} to='/login' color='black' fontWeight='bold' mx={2}>
+                        Login
+                    </ChakraLink>
+                    <ChakraLink as={Link} to='/' color='black' fontWeight='bold' mx={2}>
+                        Home
+                    </ChakraLink>
+                    <ChakraLink as={Link} to='/about' color='black' fontWeight='bold' mx={2}>
+                        About
+                    </ChakraLink>
+                    <ChakraLink as={Link} to='/menu' color='black' fontWeight='bold' mx={2}>
+                        Menu
+                    </ChakraLink>
+                    <ChakraLink as={Link} to='/reservations' color='black' fontWeight='bold' mx={2}>
+                        Reservations
+                    </ChakraLink>
+                    <ChakraLink as={Link} to='/order-online' color='black' fontWeight='bold' mx={2} whiteSpace='nowrap'>
+                        Order Online
+                    </ChakraLink>
+                    <Spacer />
+                </Flex>
+                <Box display={{ base: 'block', md: 'none' }} px={2}>
+                    <IconButton
+                        aria-label='Open menu'
+                        icon={<FaBars />}
+                        variant='ghost'
+                        onClick={onOpen}
+                        size='lg'
+                    />
+                </Box>
+
+                <Box display={{ base: isOpen ? 'block' : 'none', md: 'none' }} position='absolute' top='0' right='0' w='100%'>
+                    <Box bg='gray.100' p={2}>
+                        <Flex justify='flex-end'>
+                            <IconButton
+                                aria-label='Close menu'
+                                icon={<FaTimes />}
+                                variant='ghost'
+                                onClick={onClose}
+                                size='lg'
+                            />
+                        </Flex>
+                        <Box textAlign='center'>
+                            <HStack flexDirection='column' spacing='4'>
+                                <ChakraLink as={Link} to='/login' color='black' fontWeight='bold' mx={2} onClick={onClose}>
+                                    Login
+                                </ChakraLink>
+                                <ChakraLink as={Link} to='/' color='black' fontWeight='bold' mx={2} onClick={onClose}>
+                                    Home
+                                </ChakraLink>
+                                <ChakraLink as={Link} to='/about' color='black' fontWeight='bold' mx={2} onClick={onClose}>
+                                    About
+                                </ChakraLink>
+                                <ChakraLink as={Link} to='/menu' color='black' fontWeight='bold' mx={2} onClick={onClose}>
+                                    Menu
+                                </ChakraLink>
+                                <ChakraLink as={Link} to='/reservations' color='black' fontWeight='bold' mx={2} onClick={onClose}>
+                                    Reservations
+                                </ChakraLink>
+                                <ChakraLink as={Link} to='/order-online' color='black' fontWeight='bold' mx={2} whiteSpace='nowrap' onClick={onClose}>
+                                    Order Online
+                                </ChakraLink>
+                            </HStack>
+                        </Box>
+                    </Box>
+                </Box>
+            </Flex>
+        </Box>
+    );
+
+};
+
+export default Navbar;
