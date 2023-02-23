e80972869f2dc055bd978bbc78d14903043bef1b
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-02-22 21:53:01 -0600

    Adding nowrap to Hero Heading

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,8 +6,15 @@ const Hero = () => {
     return (
         <Box bg='brand.primary1' display='flex' justifyContent='space-evenly' p={5}>
             <VStack spacing={4} m='auto' w={200}>
-                <Text color='brand.primary2' fontWeight='extrabold' fontSize={64}>Little Lemon</Text>
-                <Text color='brand.highlight1' fontWeight='bold' fontSize={40}>Chicago</Text>
+                <Text color='brand.primary2' fontWeight='extrabold' fontSize={64} style={{ whiteSpace: 'nowrap' }}>Little Lemon</Text>
+                <Text
+                    color='brand.highlight1'
+                    fontWeight='bold'
+                    fontSize={40}
+                    textAlign="left"
+                >
+                    Chicago
+                </Text>
                 <Text color='brand.highlight1'>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</Text>
                 <Button bg='brand.primary2' p={2} borderRadius={16} fontWeight='bold'>Reserve a Table</Button>
             </VStack>
