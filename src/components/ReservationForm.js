1504d997ada889eb7fc9eb5bfa28f5d6aba183ca
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-03-01 22:14:41 -0600

    Exercise: Adding table booking state Solution

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -1,24 +1,16 @@
-import { Box, Button, FormControl, FormErrorMessage, FormLabel, Grid, GridItem, Input, Select, Text } from "@chakra-ui/react";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import { useState } from "react";
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, Grid, GridItem, Input, Select } from "@chakra-ui/react"
+import { useFormik } from "formik"
+import * as Yup from "yup"
+import { useEffect } from "react"
 
 const validationSchema = Yup.object().shape({
     date: Yup.date().required("Date is required"),
     time: Yup.string().required("Time is required"),
     guests: Yup.number().required("Number of guests is required").min(1, "Number of guests must be at least 1").max(10, "Number of guests must be at most 10"),
     occasion: Yup.string().required("Occasion is required"),
-});
+})
 
-const ReservationForm = () => {
-    const [availableTimes, setAvailableTimes] = useState([
-        "17:00",
-        "18:00",
-        "19:00",
-        "20:00",
-        "21:00",
-        "22:00"
-    ]);
+const ReservationForm = ({ updateTimes, availableTimes, setAvailableTimes }) => {
 
     const formik = useFormik({
         initialValues: {
@@ -29,16 +21,32 @@ const ReservationForm = () => {
         },
         validationSchema,
         onSubmit: (values) => {
-            console.log(values);
+            console.log(values)
         },
-    });
+    })
 
-    const handleTimeChange = (e) => {
-        formik.handleChange(e);
-        const selectedTime = e.target.value;
-        const updatedTimes = availableTimes.filter((time) => time !== selectedTime);
-        setAvailableTimes(updatedTimes);
-    };
+    useEffect(() => {
+        // simulate API call to fetch available times for the selected date
+        const fetchAvailableTimes = async () => {
+            const response = await fetch(
+                `https://myapi.com/times?date=${formik.values.date}`
+            )
+            const data = await response.json()
+            setAvailableTimes(data)
+            updateTimes() // dispatch state change to update available times
+        }
+
+        if (formik.values.date) {
+            fetchAvailableTimes()
+        }
+    }, [formik.values.date])
+
+    const handleTimeChange = (e, times) => {
+        formik.handleChange(e)
+        const selectedTime = e.target.value
+        const updatedTimes = times.filter((time) => time !== selectedTime)
+        setAvailableTimes(updatedTimes)
+    }
 
     return (
         <Box maxW={200} mx="auto">
@@ -47,14 +55,27 @@ const ReservationForm = () => {
                     <GridItem colSpan={2}>
                         <FormControl isInvalid={formik.touched.date && formik.errors.date}>
                             <FormLabel htmlFor="date">Choose date</FormLabel>
-                            <Input type="date" id="date" name="date" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.date} />
+                            <Input
+                                type="date"
+                                id="date"
+                                name="date"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.date}
+                            />
                             <FormErrorMessage>{formik.errors.date}</FormErrorMessage>
                         </FormControl>
                     </GridItem>
                     <GridItem colSpan={2}>
                         <FormControl isInvalid={formik.touched.time && formik.errors.time}>
                             <FormLabel htmlFor="time">Choose time</FormLabel>
-                            <Select id="time" name="time" onChange={handleTimeChange} onBlur={formik.handleBlur} value={formik.values.time}>
+                            <Select
+                                id="time"
+                                name="time"
+                                onChange={(e) => handleTimeChange(e, availableTimes)}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.time}
+                            >
                                 <option value="">Select a time</option>
                                 {availableTimes.map((time) => (
                                     <option key={time} value={time}>
@@ -68,26 +89,41 @@ const ReservationForm = () => {
                     <GridItem colSpan={2}>
                         <FormControl isInvalid={formik.touched.guests && formik.errors.guests}>
                             <FormLabel htmlFor="guests">Number of guests</FormLabel>
-                            <Input type="number" id="guests" name="guests" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.guests} />
+                            <Input
+                                type="number"
+                                step={1}
+                                min={1}
+                                id="guests"
+                                name="guests"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.guests}
+                            />
                             <FormErrorMessage>{formik.errors.guests}</FormErrorMessage>
                         </FormControl>
                     </GridItem>
                     <GridItem colSpan={2}>
                         <FormControl isInvalid={formik.touched.occasion && formik.errors.occasion}>
-                            <FormLabel htmlFor="occasion">Occasion</FormLabel>
-                            <Select id="occasion" name="occasion" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.occasion}>
-                                <option value="">Select an occasion</option>
-                                <option value="Birthday">Birthday</option>
-                                <option value="Anniversary">Anniversary</option>
-                                <option value="Wedding">Wedding</option>
-                                <option value="Corporate event">Corporate event</option>
-                                <option value="Other">Other</option>
-                            </Select>
+                            <FormLabel htmlFor="occasion">Occasion (optional)</FormLabel>
+                            <Input
+                                type="text"
+                                id="occasion"
+                                name="occasion"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.occasion}
+                            />
                             <FormErrorMessage>{formik.errors.occasion}</FormErrorMessage>
                         </FormControl>
                     </GridItem>
                     <GridItem colSpan={2}>
-                        <Button colorScheme="teal" type="submit" isDisabled={!formik.isValid}>Submit</Button>
+                        <Button
+                            colorScheme="teal"
+                            type="submit"
+                            disabled={!formik.isValid || formik.isSubmitting}
+                        >
+                            {formik.isSubmitting ? "Submitting..." : "Submit"}
+                        </Button>
                     </GridItem>
                 </Grid>
             </form>
@@ -95,5 +131,4 @@ const ReservationForm = () => {
     );
 };
 
-export default ReservationForm;
-
+export default ReservationForm
--- src/pages/Reservations.js
+++ src/pages/Reservations.js
@@ -1,12 +1,47 @@
-import React from 'react'
-import ReservationForm from '../components/ReservationForm'
+import { useState } from "react"
+import { Container, Heading, Box, SimpleGrid } from "@chakra-ui/react"
+import ReservationForm from "../components/ReservationForm"
 
-const Reservations = () => {
+const Reservation = () => {
+    const [availableTimes, setAvailableTimes] = useState([
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00",
+        "22:00"
+    ])
+
+    const updateTimes = (newTimes) => {
+        setAvailableTimes(newTimes)
+    }
+
     return (
-        <div>
-            <ReservationForm />
-        </div>
+        <Container>
+            <Heading as="h1" my={10}>
+                Make a Reservation
+            </Heading>
+            <Box my={5}>
+                <SimpleGrid columns={[1, null, 2]} gap={10}>
+                    <ReservationForm updateTimes={updateTimes} availableTimes={availableTimes} setAvailableTimes={setAvailableTimes} />
+                    <Box>
+                        <Heading as="h2" size="md" mb={5}>
+                            Available times:
+                        </Heading>
+                        {availableTimes.length > 0 ? (
+                            <ul>
+                                {availableTimes.map((time) => (
+                                    <li key={time}>{time}</li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <p>No available times for the selected date.</p>
+                        )}
+                    </Box>
+                </SimpleGrid>
+            </Box>
+        </Container>
     )
 }
 
-export default Reservations
+export default Reservation
