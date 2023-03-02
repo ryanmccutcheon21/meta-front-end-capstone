b2c85adb87d86acef7acaa4b6478933ba6523405
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-03-01 22:35:56 -0600

    Add unit tests for the Reservations and ReservationForm components

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react"
+import Reservations from "./pages/Reservations"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const availableTimes = [
+  "17:00",
+  "18:00",
+  "19:00",
+  "20:00",
+  "21:00",
+  "22:00"
+]
+
+test('Renders the ReservationForm heading', () => {
+  render(<ReservationForm />)
+  const heading = screen.getByText('Make a Reservation')
+  expect(heading).toBeInTheDocument()
+})
+
+describe("Reservations", () => {
+  it("returns the expected available times", () => {
+
+    const { getByText } = render(<Reservations />)
+    const availableTimes = getByText(/Available Times/i)
+
+    expect(availableTimes).toBeInTheDocument()
+    availableTimes.forEach((time) => {
+      expect(availableTimes).toHaveTextContent(time)
+    })
+  })
+
+  it("should return the same value that is provided in the state", () => {
+    const initialState = {
+      availableTimes
+    }
+
+    const { result } = render(<Reservations />, { initialState })
+
+    expect(result.current.updateTimes()).toEqual(initialState.availableTimes)
+  })
+})
